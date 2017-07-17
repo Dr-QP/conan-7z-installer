0388b9634c0b1861ad407a80755f7b005beb86d0
Anton Matosov <anton.matosov@gmail.com>
2017-07-16 23:22:17 -0700

    Fix names and crossbuild using this package

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,20 @@
-from conans import ConanFile
+from conans import ConanFile, tools
 import os
 
 
 class SevenZinstallerConan(ConanFile):
-    name = "7z_installer"
-    version = "0.1"
+    name = "7z-installer"
+    version = "16.02"
     license = "GNU LGPL + unRAR restriction"
-    url = "http://github.com/lasote/conan-7z-installer"
-    settings = {"os": ["Windows"]}
+    url = "https://github.com/Dr-QP/conan-7z-installer"
+    description = "7-Zip is a file archiver for Windows NT / 2000 / 2003 / 2008 / 2012 / XP / Vista / 7 / 8 / 10"
+    settings = None
     exports = "sources/*"
     build_policy = "missing"
+
+    def configure(self):
+        if not tools.os_info.is_windows:
+            raise Exception("7z-installer package is designed for use on Windows host only")
 
     def package(self):
         self.copy("*", dst="bin", src="sources")
